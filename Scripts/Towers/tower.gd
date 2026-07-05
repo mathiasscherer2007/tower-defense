163df913dac6593f24fe2f9407b99ecdd59dc24d
Mathias <mathischerer@gmail.com>
2026-07-04 23:17:07 -0300

    Add Tower base class, remove Bullets node from level and add it to Tower scene

---
+++ Scripts/Towers/tower.gd
@@ -0,0 +1,29 @@
+class_name Tower extends Node3D
+
+# TODO: more robust targeting system, make tower shoot enemy that is furthest in the track
+
+var enemies: Array[Enemy]
+@export var max_range: float
+@onready var range_area = $Range/CollisionShape3D
+@onready var mesh = $MeshInstance3D
+@onready var timer = $Firerate
+
+func _ready() -> void:
+	if range_area.shape is CylinderShape3D:
+		range_area.shape.radius = max_range
+	print(range_area.shape.radius)
+
+func _process(_delta: float) -> void:
+	if timer.is_stopped():
+		if len(enemies) > 0:
+			var enemy_pos: Vector3 = enemies[0].global_position
+			mesh.look_at(Vector3(enemy_pos.x, self.global_position.y, enemy_pos.z))
+			timer.start()
+
+func _on_range_area_entered(area: Area3D) -> void:
+	if area not in enemies:
+		enemies.append(area)
+
+func _on_range_area_exited(area: Area3D) -> void:
+	if area in enemies:
+		enemies.erase(area)
